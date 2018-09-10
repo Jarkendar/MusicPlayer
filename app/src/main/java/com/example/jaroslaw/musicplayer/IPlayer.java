123a7e3b4dbbf510f06022f5bdc938298fe62de4
jaroslaw <jarek.skrzypczak@o2.pl>
2018-09-10 13:56:37 +0200

    Add Player interface

---
+++ app/src/main/java/com/example/jaroslaw/musicplayer/IPlayer.java
@@ -0,0 +1,13 @@
+package com.example.jaroslaw.musicplayer;
+
+public interface IPlayer {
+    public void start();
+    public void chooseAndPlay(String path);
+    public void stop();
+    public void next();
+    public void previous();
+    public void refreshSong();
+    public void changeMode();
+    public void getShortListPlayed();
+    public void getListPlayed();
+}
