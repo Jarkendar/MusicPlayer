2e16fcc618a4eb44b34d4b4e97a2bb928edbaaf4
jaroslaw <jarek.skrzypczak@o2.pl>
2018-09-10 14:11:21 +0200

    Add variables to player

--- app/src/main/java/com/example/jaroslaw/musicplayer/Player.java
+++ app/src/main/java/com/example/jaroslaw/musicplayer/Player.java
@@ -1,6 +1,27 @@
 package com.example.jaroslaw.musicplayer;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+
+import java.util.LinkedList;
+import java.util.logging.Handler;
+
 public class Player implements IPlayer {
+
+    private Context context;
+    private DataBaseLackey dataBaseLackey; //todo new table for list play
+    private LinkedList<Track> willBePlayed;//todo 10 songs
+    private Track currentPlay;
+    private LinkedList<Track> history;// todo 50 songs
+    private Mode mode;
+    private MediaPlayer mediaPlayer;
+    private Handler handler;
+
+    public Player(Context context) {
+        this.context = context;
+        dataBaseLackey = new DataBaseLackey(context);
+        mediaPlayer = new MediaPlayer();
+    }
 
     @Override
     public void start() {
