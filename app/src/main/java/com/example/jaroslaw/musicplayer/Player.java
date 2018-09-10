6747f84309efbd799e68bf4431eb36a1e3ecf91b
jaroslaw <jarek.skrzypczak@o2.pl>
2018-09-10 13:57:08 +0200

    Add simple Player

---
+++ app/src/main/java/com/example/jaroslaw/musicplayer/Player.java
@@ -0,0 +1,49 @@
+package com.example.jaroslaw.musicplayer;
+
+public class Player implements IPlayer {
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void chooseAndPlay(String path) {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void next() {
+
+    }
+
+    @Override
+    public void previous() {
+
+    }
+
+    @Override
+    public void refreshSong() {
+
+    }
+
+    @Override
+    public void changeMode() {
+
+    }
+
+    @Override
+    public void getShortListPlayed() {
+
+    }
+
+    @Override
+    public void getListPlayed() {
+
+    }
+}
