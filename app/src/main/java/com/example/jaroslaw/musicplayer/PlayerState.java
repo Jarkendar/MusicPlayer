7953e841a8eb8dcb86151c036773d035072913da
jaroslaw <jarek.skrzypczak@o2.pl>
2018-09-12 20:33:49 +0200

    Add PlayerState to store state history, current, next songs

---
+++ app/src/main/java/com/example/jaroslaw/musicplayer/PlayerState.java
@@ -0,0 +1,41 @@
+package com.example.jaroslaw.musicplayer;
+
+import java.util.LinkedList;
+
+public class PlayerState {
+    private LinkedList<Track> history;
+    private Track current;
+    private LinkedList<Track> next;
+
+    public PlayerState() {}
+
+    public PlayerState(LinkedList<Track> history, Track current, LinkedList<Track> next) {
+        this.history = history;
+        this.current = current;
+        this.next = next;
+    }
+
+    public LinkedList<Track> getHistory() {
+        return history;
+    }
+
+    public Track getCurrent() {
+        return current;
+    }
+
+    public LinkedList<Track> getNext() {
+        return next;
+    }
+
+    public void addLastHistory(Track track){
+        history.addLast(track);
+    }
+
+    public void addLastNext(Track track){
+        next.addLast(track);
+    }
+
+    public void setCurrent(Track track){
+        current = track;
+    }
+}
